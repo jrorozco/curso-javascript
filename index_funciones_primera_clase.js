71ec6f2c7921d641951fce86b384da538ead38f5
Rene Orozco <rene@alvaroquintana.com>
2018-01-12 12:46:20 -0600

    funciones de primera clase

---
+++ index_funciones_primera_clase.js
@@ -0,0 +1,18 @@
+/**
+ * Funciones de primera clase...
+ */
+
+ function a()
+ {
+     console.log("Funcion a");
+ }
+ a();
+ a.nombre="Maria";
+ a.direccion={
+     pais:"Costa Rica",
+     ciudad:"San Jose",
+     edificio:{
+         piso:"8Vo",
+         nombre:"Edificio Principal"
+     }
+ }
